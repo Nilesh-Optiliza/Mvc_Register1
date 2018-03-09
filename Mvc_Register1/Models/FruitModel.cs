0a0e7440a1b28e4e795b2256ecdd8d40726c0884
Nilesh-Optiliza <nbhavsar003@gmail.com>
2018-03-09 21:49:05 +0530

    Initial commit

---
+++ Mvc_Register1/Models/FruitModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Register1.Models
+{
+    public class FruitModel
+    {
+        public List<SelectListItem> Fruits { get; set; }
+        public int? FruitId { get; set; }
+        public string  StudentName { get; set; }
+        public bool Gender { get; set; }
+
+    }
+}
